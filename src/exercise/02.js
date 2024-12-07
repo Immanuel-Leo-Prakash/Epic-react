a3ec4889920a6342d1df56cb88b8740872499805
Immanuel Leo Prakash <immanuel.sm@zohocorp.com>
2024-12-07 20:29:56 +0530

    Lazy state initialization, useEffect usage & custom Hook creation

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -1,16 +1,18 @@
-// useEffect: persistent state
-// http://localhost:3000/isolated/exercise/02.js
-
 import * as React from 'react'
 
-function Greeting({initialName = ''}) {
-  // 🐨 initialize the state to the value from localStorage
-  // 💰 window.localStorage.getItem('name') ?? initialName
-  const [name, setName] = React.useState(initialName)
+function useLocalStorageWithState(key, defaultValue = '') {
+  const [state, setState] = React.useState(
+    () => window.localStorage.getItem(key) ?? defaultValue,
+  )
 
-  // 🐨 Here's where you'll use `React.useEffect`.
-  // The callback should set the `name` in localStorage.
-  // 💰 window.localStorage.setItem('name', name)
+  React.useEffect(() => {
+    window.localStorage.setItem(key, state)
+  }, [key, state])
+
+  return [state, setState]
+}
+function Greeting({initialName = ''}) {
+  const [name, setName] = useLocalStorageWithState('name', initialName)
 
   function handleChange(event) {
     setName(event.target.value)
@@ -27,7 +29,7 @@ function Greeting({initialName = ''}) {
 }
 
 function App() {
-  return <Greeting />
+  return <Greeting initialName="George" />
 }
 
 export default App
